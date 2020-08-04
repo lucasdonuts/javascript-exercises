b0c93abcabe858ee2abfed91cf56759c7aaddcdc
Lucas <lucasdonuts@gmail.com>
2020-08-03 20:08:19 -0500

    Finished tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let newTemp = (fTemp - 32) * (5/9);
+  return Math.round(newTemp * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let newTemp = (cTemp * 9)/5 + 32;
+  return Math.round(newTemp * 10) / 10;
 }
 
 module.exports = {
